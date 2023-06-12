c1cd40c2e4cbc0b6d92ae5c993a5afcdef342645
ruvimbo_mambinge <ruemee93@gmail.com>
2023-06-12 16:47:47 +0200

    comments

--- Merge _Strings.ts
+++ Merge _Strings.ts
@@ -1,8 +1,10 @@
 function mergeAlternately(word1: string, word2: string): string {
 
+//  will return an array ['p', 'q', 'r'] containing each character of the string.
     const wordArray1  = word1.split('')
     console.log(wordArray1)
 
+//  will return an array ['p', 'q', 'r'] containing each character of the string.
     const WordArray2 = word2.split('')
     console.log(WordArray2)
 
@@ -11,8 +13,9 @@ function mergeAlternately(word1: string, word2: string): string {
     for(let i=0; i<wordArray1.length || i<WordArray2.length; i++) {
         wordArray3.push(wordArray1[i] as never, WordArray2[i] as never);
         }
-    
- 
+        
+ // Joining the characters in wordArray3 into a single string
+ //remove commas from the string
     return wordArray3.join('')
 };
 
